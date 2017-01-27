7b34f5355f61ef5cb92924b80b21cddadf7d462a
lk <lucaskeys@gmail.com>
2017-01-26 18:54:00 -0800

    Create keyboard and member models with constructors

--- src/app/member.model.ts
+++ src/app/member.model.ts
@@ -1,2 +1,6 @@
+import { Keyboard } from './keyboard.model';
+
 export class Member {
+  memberCount: number = 0;
+  constructor(public name: string, public title: string, public ownedKeyboards: Keyboard[], public favoriteKeyboards: Keyboard[], public joinDate: Date) {}
 }
